8d268c31c7070b8394b455ffb762ac21c02835bf
ThomasMujica <temc.business@gmail.com>
2024-09-15 20:48:54 -0300

    Solucionando problema en el header

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,5 +1,9 @@
 export const mainMenu = [
-  { link: "#", content: "Inicio", icon: "lucide:home" },
+  {
+    link: "https://temcdesign.github.io/portafolio/",
+    content: "Inicio",
+    icon: "lucide:home",
+  },
   { link: "#about", content: "Sobre mi", icon: "octicon:person-24" },
   { link: "#portafolio", content: "Portafolio", icon: "carbon:portfolio" },
   //{ link: "#blog", content: "Blog", icon: "mdi:blog" },
